01fa62cad814a7e5c7339b9cdd7bf647d7a4cd77
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-18 00:42:20 +0200

    Introduction.

---
+++ src/MsSqlFsBlobProvider/SqlFileStreamData.cs
@@ -0,0 +1,11 @@
+﻿namespace SenseNet.MsSqlFsBlobProvider
+{
+    /// <summary>
+    /// Holds context information for SqlFileStream operations.
+    /// </summary>
+    internal class SqlFileStreamData
+    {
+        public string Path { get; set; }
+        public byte[] TransactionContext { get; set; }
+    }
+}
