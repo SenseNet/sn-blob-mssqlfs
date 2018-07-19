c78f8d70a4373ccd176a4e99aba3e0b4b11f05e2
tusmester <tusmester@gmail.com>
2018-07-19 11:13:16 +0200

    Add script file for build and raise Tools reference to 3.0.1.

---
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -0,0 +1,6 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+
+# delete existing packages
+Remove-Item $PSScriptRoot\*.nupkg
+
+nuget pack $srcPath\MsSqlFsBlobProvider\SenseNet.MsSqlFsBlobProvider.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
