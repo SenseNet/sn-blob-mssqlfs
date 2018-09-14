9e37bae4944c72378bd5d0db5a797dc6afa79655
tusmester <tusmester@gmail.com>
2018-09-14 10:01:31 +0200

    Raise version to 1.0.2.

--- src/MsSqlFsBlobProvider/Properties/AssemblyInfo.cs
+++ src/MsSqlFsBlobProvider/Properties/AssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7ec0d2c0-9e97-4c60-bf23-fe4e3fa5bfc8")]
